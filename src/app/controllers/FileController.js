802cb4f3e279d6bf280a8351911140e86ec70a9f
murilokrugner <murilokrugner@hotmail.com>
2020-09-27 23:09:45 -0300

    ...

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -2,9 +2,7 @@ import File from '../models/File';
 
 class FileController {
   async store(req, res) {
-    const { originalname: name, filename: path } = req.file;
-
-    console.log(req.file);
+    const { originalname: name, filename: path } = req.body.body._parts[0][1];
 
     const file = await File.create({
       name,
